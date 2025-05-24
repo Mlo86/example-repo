81f65c16d978b3d8449fe498fe44e2fdc36019c1
Mlo86 <mlo@pjc.co.za>
2025-05-24 15:33:57 +0200

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# Ask user their name and print it 
+# Ask user their age and print it 
+# Print "Hello. World"
+
+name = input ("Enter your name: ")
+print("Your name is", name)
+
+age = input ("Enter your age: ")
+print("Your age is", age)
+
+print("Hello, World)")
+
